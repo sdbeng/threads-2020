4641e43f887dcf0ec6a3c409d8b7b971312558c3
Sergio Barrera <sdbeng@gmail.com>
2019-07-16 14:43:21 -0700

    Add app.py

---
+++ app.py
@@ -0,0 +1,10 @@
+import threading
+
+class mythread(threading.Thread):
+  def __init__(self, i):
+    threading.Thread.__init__(self)
+    self.h = i
+def run(self):
+  print ("Value send", self.h)
+thread1 = mythread(1)
+thread1.start()
